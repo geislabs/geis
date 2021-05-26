80567942dfcf84949b93c7516aea0d074f0034d3
mads hargreave <mads.hargreave@gmail.com>
2021-05-26 12:29:13 +0200

    released

---
+++ packages/geis-html/lib/images/index.ts
@@ -0,0 +1,2 @@
+export * from './imageAdapter'
+export * from './imageTypes'
--- packages/geis-main/lib/index.ts
+++ packages/geis-main/lib/index.ts
@@ -1,6 +1,9 @@
+export * from '@geislabs/geis-file'
 export { browse, mock } from '@geislabs/geis-browse'
 export { cast, apply } from '@geislabs/geis-type'
-export { String, Integer } from '@geislabs/geis-core'
+export { String, Integer, Boolean } from '@geislabs/geis-core'
+export { Link } from '@geislabs/geis-html'
+export { Image } from '@geislabs/geis-image'
 
 export * from './mainFacade'
 export * from './mainTypes'
