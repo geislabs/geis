be337022abf4b69afd703bf4dbc594767a4d5271
mads hargreave <mads.hargreave@gmail.com>
2021-05-28 10:07:41 +0200

    Json type

--- packages/geis-fetch/lib/node/nodeFacade.ts
+++ packages/geis-fetch/lib/node/nodeFacade.ts
@@ -4,6 +4,7 @@ import { FetchFn } from '../fetchConfig'
 export const nodeFetch: FetchFn<unknown> = async (request) => {
     const response = await fetch(request.url.toString(), {
         method: request.method,
+        body: request.body,
     })
     return { body: response.body }
 }
