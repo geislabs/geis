2542a38900a9a9006da75a60d73391ccd80d8bb7
mads hargreave <mads.hargreave@gmail.com>
2021-06-02 13:34:38 +0200

    released

--- examples/promise.js
+++ examples/promise.js
@@ -5,37 +5,22 @@
  * This sample code illustrates how to scrape the results of a webpage
  */
 
-const { default: config } = require('..') // const config = require('@geislabs/geis')
-const { puppeteer } = require('../packages/geis-puppeteer/dist')
+const { config } = require('..') // const config = require('@geislabs/geis')
 
-const { run, watch } = config({
-    plugins: [
-        proxy({
-            mapping: {
-                'google.com': 'localhost:4000',
-            },
-        }),
-    ],
-})
-
-const count = Count(0)
-
-const source = run([1, 2, 3], function* (value, index, { browse }) {
-    const home = browse('https://github.com/geislabs/geis')
-    yield {
-        value1: Promise.resolve(home),
-        value2: Promise.resolve(home),
-        value3: Promise.resolve(home),
-    }
-})
-
-const source = run(function* ({ browse }) {
-    for (const value of [1, 2, 3]) {
-        const home = browse('https://github.com/geislabs/geis')
-        yield {
-            value1: Promise.resolve(home),
-            value2: Promise.resolve(home),
-            value3: Promise.resolve(home),
+async function run() {
+    const geis = config()
+    const source1 = geis.run(
+        [1, 2, 3, 4, 5],
+        async function* (value, index, { http }) {
+            const response = await http.request({
+                url: 'https://github.com/geislabs/geis',
+            })
+            yield response
         }
+    )
+    for await (const value of source1) {
+        console.log(value.status)
     }
-})
+}
+
+run()
